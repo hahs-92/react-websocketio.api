4bfd7000461bf8644ad82f09c7e2e70f6f0f3c58
hugo.hernandez <hugo.hernandez@sofka.com.co>
2023-01-20 12:19:34 -0500

    receive and send messages

--- src/index.js
+++ src/index.js
@@ -9,11 +9,26 @@ import { PORT } from "./config/index.js";
 const app = express();
 
 const server = http.createServer(app);
-const io = new SocketServer(server);
+const io = new SocketServer(server, {
+  cors: {
+    origin: "*",
+  },
+});
 
 app.use(morgan("dev"));
 app.use(cors());
 
-app.listen(PORT, () => {
+// esta eschando eventos
+io.on("connection", (socket) => {
+  console.log("user connected¡ with id: " + socket.id);
+
+  //recibimos un evento desde el cliente
+  socket.on("message", (message) => {
+    // emitimos un evento a todos los clientes
+    socket.broadcast.emit("message", message);
+  });
+});
+
+server.listen(PORT, () => {
   console.log(`app listening in port: ${PORT}`);
 });
